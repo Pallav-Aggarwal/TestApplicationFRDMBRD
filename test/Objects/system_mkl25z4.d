7e3c224c12445c15453fdcc1068c1d850dc09ffe
Pallav Aggarwal <pallav.aggarwal@gmail.com>
2015-08-21 18:26:43 +0530

    another test app
    test app

--- test/Objects/system_mkl25z4.d
+++ test/Objects/system_mkl25z4.d
@@ -1,4 +1,4 @@
-.\objects\system_mkl25z4.o: RTE\Device\MKL25Z64xxx4\system_MKL25Z4.c
+.\objects\system_mkl25z4.o: RTE\Device\MKL25Z128xxx4\system_MKL25Z4.c
 .\objects\system_mkl25z4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\system_mkl25z4.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.6.0\Device\Include\MKL25Z4.h
 .\objects\system_mkl25z4.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.3.0\CMSIS\Include\core_cm0plus.h
